5116e3753bfb7619fa12fa93d45f118466232a60
Azizbek Arzikulov <azizbek.dev.ac@gmail.com>
2025-07-04 15:23:17 +0900

    feat: SASS integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,19 @@ import { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <Stack sx={{ background: "#81c784" }}>HEADER</Stack>
-      <Container>
-        <Stack flexDirection={"column"} >
-          <Box>Popular Properties</Box>
-          <Box>Top Agents</Box>
-          <Box>Top Properties</Box>
-          <Box>Events</Box>
+    <Stack>
+      <Stack flexDirection={"column"}>
+        <Stack>
+          <Stack className="container">Popular Properties</Stack>
         </Stack>
-      </Container>
-      <Stack sx={{ background: "#a1887f" }}>Footer</Stack>
-    </>
+        <Stack>
+          <Stack className="container">Top Agents</Stack>
+        </Stack>
+        <Stack>
+          <Stack className="container">Top Properties</Stack>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
 
